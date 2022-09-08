f334c47a496edfe580054e0d0a33c58d0e9c34b6
dlsduddl <inyoung106@naver.com>
2022-09-09 03:56:40 +0900

    20220909 배너,섹션1,3 반응형 마크업

--- js/script.js
+++ js/script.js
@@ -47,10 +47,53 @@ document.querySelector('.button-right-1').addEventListener('click',function(){
   document.querySelector('.section-slide-1').style.transform = 'translate(-665px)';
 })
 
+// 반응형 태블릿
+document.querySelector('.button-right-1m').addEventListener('click',function(){
+  document.querySelector('.section-slide-1').style.transform = 'translate(-530px)';
+})
+
+// 반응형 스마트폰 
+document.querySelector('.s-button1').addEventListener('click',function(){
+  document.querySelector('.section-slide-1').style.transform = 'translate(0)';
+})
+document.querySelector('.s-button2').addEventListener('click',function(){
+  document.querySelector('.section-slide-1').style.transform = 'translate(-330px)';
+})
+document.querySelector('.s-button3').addEventListener('click',function(){
+  document.querySelector('.section-slide-1').style.transform = 'translate(-655px)';
+})
+document.querySelector('.s-button4').addEventListener('click',function(){
+  document.querySelector('.section-slide-1').style.transform = 'translate(-980px)';
+})
+document.querySelector('.s-button5').addEventListener('click',function(){
+  document.querySelector('.section-slide-1').style.transform = 'translate(-1305px)';
+})
+
 // section-3-2 슬라이드 버튼
 document.querySelector('.button-left-3').addEventListener('click',function(){
   document.querySelector('.section-slide-3').style.transform = 'translate(0)';
 })
 document.querySelector('.button-right-3').addEventListener('click',function(){
   document.querySelector('.section-slide-3').style.transform = 'translate(-665px)';
+})
+
+// 반응형 태블릿
+document.querySelector('.button-right-3m').addEventListener('click',function(){
+  document.querySelector('.section-slide-3').style.transform = 'translate(-530px)';
+})
+// 반응형 스마트폰 
+document.querySelector('.s-button3-1').addEventListener('click',function(){
+  document.querySelector('.section-slide-3').style.transform = 'translate(0)';
+})
+document.querySelector('.s-button3-2').addEventListener('click',function(){
+  document.querySelector('.section-slide-3').style.transform = 'translate(-330px)';
+})
+document.querySelector('.s-button3-3').addEventListener('click',function(){
+  document.querySelector('.section-slide-3').style.transform = 'translate(-655px)';
+})
+document.querySelector('.s-button3-4').addEventListener('click',function(){
+  document.querySelector('.section-slide-3').style.transform = 'translate(-980px)';
+})
+document.querySelector('.s-button3-5').addEventListener('click',function(){
+  document.querySelector('.section-slide-3').style.transform = 'translate(-1305px)';
 })
